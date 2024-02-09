76dff8834b9111c52afb00d089e7e6d9be05bce1
philippeapra <philippeaprahamian6@gmail.com>
2024-02-08 22:51:52 -0500

    6

--- project/collect_color_sensor_data.py
+++ project/collect_color_sensor_data.py
@@ -12,7 +12,7 @@ from utils.brick import EV3ColorSensor, wait_ready_sensors, TouchSensor,wait_rea
 COLOR_SENSOR_DATA_FILE = "../data_analysis/color_sensor.csv"
 
 # complete this based on your hardware setup
-COLOR_SENSOR = EV3ColorSensor(3)
+COLOR_SENSOR = EV3ColorSensor(4)
 TOUCH_SENSOR = TouchSensor(1)
 
 wait_ready_sensors(True) # Input True to see what the robot is trying to initialize! False to be silent.
@@ -20,26 +20,29 @@ wait_ready_sensors(True) # Input True to see what the robot is trying to initial
 
 def collect_color_sensor_data():
     "Collect color sensor data."
-    not_pressed = TOUCH_SENSOR.get_status
     curr_pressed = False
+    counter=0
     try:
         output_file = open(COLOR_SENSOR_DATA_FILE, "w")
         while True:
-            if not TOUCH_SENSOR.is_pressed():
+            if counter == 10 :
+                break
+            if (not TOUCH_SENSOR.is_pressed()):
                 curr_pressed = False
-            elif TOUCH_SENSOR.is_pressed() and curr_pressed==False :
+                
+            elif TOUCH_SENSOR.is_pressed() and curr_pressed == False :
                 curr_pressed = True
                 color_data = COLOR_SENSOR.get_rgb()
                 if color_data is not None: # If None is given, then data collection failed that time
-                    print("Touch sensor pressed")
-                    print(color_data)
                     output_file.write(f"{color_data}\n")
+                    counter = counter + 1
+                    
                     
                     
     except BaseException:  # capture all exceptions including KeyboardInterrupt (Ctrl-C)
         pass
     finally:
-        print("Done collecting US distance samples")
+        print("Done collecting color data samples")
         output_file.close()
         reset_brick() # Turn off everything on the brick's hardware, and reset it
         exit()
