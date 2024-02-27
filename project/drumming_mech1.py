39521098f819742c5bd89aa1c5ab4b2c1697ead1
philippeapra <philippeaprahamian6@gmail.com>
2024-02-26 21:58:36 -0500

    final code lab 3b

--- project/drumming_mech1.py
+++ project/drumming_mech1.py
@@ -17,12 +17,12 @@ wait_ready_sensors()
 print("Done initializing code")
 
 # Initialize tone sounds
-tone1 = Sound(duration=3, volume=80, pitch="C5")
-tone2 = Sound(duration=3, volume=80, pitch="G5")
-tone3 = Sound(duration=3, volume=80, pitch="A5")
-tone4 = Sound(duration=3, volume=80, pitch="F5")
-tone5 = Sound(duration=3, volume=80, pitch="E5")
-tone6 = Sound(duration=3, volume=80, pitch="D5")
+tone1 = Sound(duration=3, volume=90, pitch="C5")
+tone2 = Sound(duration=3, volume=90, pitch="G5")
+tone3 = Sound(duration=3, volume=90, pitch="A5")
+tone4 = Sound(duration=3, volume=90, pitch="F5")
+tone5 = Sound(duration=3, volume=90, pitch="E5")
+tone6 = Sound(duration=3, volume=90, pitch="D5")
 print("Done initializing sounds")
 
 def stop_all_sound():
@@ -50,11 +50,15 @@ def update_pressed_str():
         else:
             pressed+="0"
 
+        if DRUM_SENSOR.is_pressed():
+            pressed+="1"
+        else:
+            pressed+="0"
+
 try:
     
     print("Starting main loop")
     DRUM_STARTED = False
-    touch_sensors = [TS1,TS2,TS3]
     pressed = ""   #string expressing which btns are pressed in this iteration
     
     
@@ -62,41 +66,35 @@ try:
 
         pressed = update_pressed_str()
 
-
         # Emergency stop mechanism
-        if pressed=="111":
+        if pressed=="1111":
             break
 
         # Drumming mechanism
         if DRUM_SENSOR.is_pressed() and not DRUM_STARTED:
             DRUM_MOTOR.set_power(100)
             DRUM_STARTED = True
-            time.sleep(0.25)
-        if DRUM_SENSOR.is_pressed() and DRUM_STARTED:
-            DRUM_MOTOR.set_power(0)
-            DRUM_STARTED = False
-            time.sleep(0.25)
-
+            time.sleep(0.1)
         # Flute mechanism
-        if pressed=="100":
+        elif pressed=="1000":
             stop_all_sound()
             tone1.play()
-        elif pressed=="010":
+        elif pressed=="0100":
             stop_all_sound()
             tone2.play()
-        elif pressed=="001":
+        elif pressed=="0010":
             stop_all_sound()
             tone3.play()
-        elif pressed=="110":
+        elif pressed=="1100":
             stop_all_sound()
             tone4.play()
-        elif pressed=="011":
+        elif pressed=="0110":
             stop_all_sound()
             tone5.play()
-        elif pressed=="101":
+        elif pressed=="1010":
             stop_all_sound()
             tone6.play()
-        elif pressed=="000":
+        elif pressed=="0000":
             stop_all_sound()
 
 # capture all exceptions
